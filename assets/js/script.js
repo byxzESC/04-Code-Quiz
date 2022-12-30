ca0fd616a2bcd079fa7b338c1d9c246f2714b553
byxz885@gmail.com <53200767+byzESC@users.noreply.github.com>
2022-12-29 20:18:00 -0800

    testing questions mechanics

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,75 @@
 
 // WHEN the game is over
 // THEN I can save my initials and score
+
+// variable to keep score, initials, different section of html 
+// listener for start button
+var startElement = document.getElementById("start-section");
+var startButton = document.getElementById("start-btn");
+
+var quizElement = document.getElementById("quiz-section");
+var questionElement = document.getElementById("question");
+var choicesElement = document.getElementById("choices");
+
+// Questions are an array of question objects
+var questions = [
+    {
+        question: 'what is 1 + 1?',
+        choice1: '2',
+        choice2: '3',
+        choice3: '4',
+        choice4: '5',
+        answer: 1,
+    },
+    {
+        question: 'what is 2 + 2?',
+        choice1: '2',
+        choice2: '3',
+        choice3: '4',
+        choice4: '5',
+        answer: 3,
+    }
+]
+
+// function for starting the quiz
+function startQuiz () {
+    hideShow(startElement, quizElement);
+}
+
+// function for selecting answer
+function selectAnswer() {
+
+}
+
+
+// function to hide and show start and quiz elements
+function hideShow (hide, show) {
+    hide.classList.replace("visible", "hidden");
+    show.classList.replace("hidden", "visible");
+}
+
+startButton.addEventListener("click", function () {
+    console.log("game start");
+    startQuiz();
+});
+
+
+
+
+
+// function that starts the quiz 
+// startButton.addEventListener("click", function() {
+//     console.log('start button clicked!');
+//     hideShow(startElement, quizElement)
+//     quiz();
+// })
+
+    // // get the element by its ID to add or remove, the hidden or visible class.
+    // // if hidden argument doesn't have hidden
+    // if (Object.values(hidden.classList).includes('hidden')) {
+        
+    // }
+    //     // add hidden class to the hidden argument  
+    //     console.log(hidden.classList);
+    //     // add visible class to the visible argument
+    // // else replace their classes
